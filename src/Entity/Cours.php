1c9f4ded23c6feb61194bdf5584054625e49dee8
Ericphoto35 <150171033+Ericphoto35@users.noreply.github.com>
2024-07-03 20:48:02 +0200

    push

--- src/Entity/Cours.php
+++ src/Entity/Cours.php
@@ -16,6 +16,9 @@ class Cours
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $debutant = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $inter = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +32,18 @@ class Cours
     public function setDebutant(?string $debutant): static
     {
         $this->debutant = $debutant;
+
+        return $this;
+    }
+
+    public function getInter(): ?string
+    {
+        return $this->inter;
+    }
+
+    public function setInter(?string $inter): static
+    {
+        $this->inter = $inter;
 
         return $this;
     }
